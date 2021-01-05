90bed75d6e70576533823466d7cef0537fd45cd2
YdrMaster <ydrml@hotmail.com>
2021-01-05 02:32:30 +0000

    fix: 调整状态设计的 bug

--- src/host/host_t_receive.cpp
+++ src/host/host_t_receive.cpp
@@ -6,6 +6,8 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 
+#include <iostream>
+
 namespace autolabor::connection_aggregation
 {
     bool srand_t::check_received(uint16_t id)
@@ -74,12 +76,18 @@ namespace autolabor::connection_aggregation
         // 只能在一个线程调用，拿不到锁则放弃
         TRY_LOCK(_receiving, return );
 
-        auto now = std::chrono::steady_clock::now();
-        epoll_event events[128];
+        auto SOURCE = reinterpret_cast<const in_addr *>(buffer);       // 解出源虚拟地址
+        auto TYPE = reinterpret_cast<const pack_type_t *>(SOURCE + 1); // 解出类型字节
+
+        // auto t1 = std::chrono::steady_clock::now();
+        epoll_event events[8];
         while (true)
         {
             // 阻塞
+            // auto now = std::chrono::steady_clock::now();
+            // std::cout << "R " << std::chrono::duration<float, std::micro>(now - t1).count() << "us" << std::endl;
             auto event_count = epoll_wait(_epoll, events, sizeof(events) / sizeof(epoll_event), -1);
+            // t1 = std::chrono::steady_clock::now();
             if (event_count == 0)
                 continue;
             if (event_count < 0)
@@ -103,21 +111,21 @@ namespace autolabor::connection_aggregation
                 }
 
                 connection_key_union _union{.pair{.src_index = index, .dst_port = remote.sin_port}};
-                auto source = reinterpret_cast<in_addr *>(buffer);       // 解出源虚拟地址
-                auto type = reinterpret_cast<pack_type_t *>(source + 1); // 解出类型字节
-                add_remote(*source, remote.sin_port, remote.sin_addr);
-                send_handshake(*source);
+
+                const auto source = *SOURCE;
+                const auto type = *TYPE;
+                add_remote(source, remote.sin_port, remote.sin_addr);
                 {
                     READ_LOCK(_srand_mutex);
-                    auto &s = _srands.at(source->s_addr);
+                    auto &s = _srands.at(source.s_addr);
                     read_lock lc(s.connection_mutex);
-                    s.connections.at(_union.key).received_once(type->state);
-                    if (type->forward)
+                    s.connections.at(_union.key).received_once(type.state);
+                    if (type.forward)
                     {
                         // 转发包中有一个 ip 头
                         auto ip_ = reinterpret_cast<ip *>(buffer);
                         // 用于去重的连接束序号存在 sum 处
-                        if (!type->multiple || s.check_received(ip_->ip_sum))
+                        if (!type.multiple || s.check_received(ip_->ip_sum))
                         {
                             if (ip_->ip_dst.s_addr == _address.s_addr)
                             {
@@ -136,12 +144,13 @@ namespace autolabor::connection_aggregation
                                 forward_inner(buffer, n);
                         }
                     }
-                    else if (type->multiple)
+                    else if (type.multiple)
                     { // 需要去重
                         if (n >= 4 && s.check_received(reinterpret_cast<uint8_t *>(buffer)[1]))
                             ; // TODO 使用包内容
                     };
                 }
+                send_handshake(source);
             }
         }
     }
